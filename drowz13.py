3f8e8683da18f739ae76b6418d8cdbcb53ce5b10
gopika452 <gopikasn635@gmail.com>
2024-11-26 16:16:58 +0530

    Add files via upload

---
+++ drowz13.py
@@ -0,0 +1,66 @@
+import cv2
+import imutils
+from imutils import face_utils
+import dlib
+from scipy.spatial import distance
+from pygame import mixer
+
+mixer.init()
+mixer.music.load("music.wav")
+
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    return (A + B) / (2.0 * C)
+
+thresh = 0.25
+flag = 0
+frame_check = 20
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS['left_eye']
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS['right_eye']
+
+detect = dlib.get_frontal_face_detector()
+predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    subjects = detect(gray, 0)
+    
+    for subject in subjects:
+        shape = predict(gray, subject)
+        shape = face_utils.shape_to_np(shape)
+        
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEar = eye_aspect_ratio(leftEye)
+        rightEar = eye_aspect_ratio(rightEye)
+        ear = (leftEar + rightEar) / 2.0
+        
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        
+        if ear < thresh:
+            flag += 1
+            if flag >= frame_check:
+                cv2.putText(frame, "'''''ALERT'''''", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                cv2.putText(frame, "'''''ALERT'''''", (10, 325), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                mixer.music.play()
+        else:
+            flag = 0
+
+    cv2.imshow("Frame", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
